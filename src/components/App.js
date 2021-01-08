42b794825341eebc24af6a227506dced62b80b6c
Farkhat Saliyev <saliyev.farkhat@gmail.com>
2021-01-08 14:00:47 -0800

    React_Webapck_starter

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h1>React App</h1>
+                <p>Starter pack</p>
+            </div>
+        )
+    }
+}
+export default App 
